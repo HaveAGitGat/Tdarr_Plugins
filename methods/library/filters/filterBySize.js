a051792e02e703a7d690f113c2081469a3bb632f
HaveAGitGat <43864057+HaveAGitGat@users.noreply.github.com>
2019-11-29 19:27:26 +0000

    [New]  Filter by size

---
+++ methods/library/filters/filterBySize.js
@@ -0,0 +1,34 @@
+
+
+function filterBySize(file, lowerBound, upperBound) {
+
+    try {
+
+        if (file.file_size / 1000 >= lowerBound && file.file_size / 1000 <= upperBound) {
+
+            var response = {
+                outcome: true,
+                note: `☑File size is within filter limits. \\n`
+            }
+            return response
+
+        } else {
+
+            var response = {
+                outcome: false,
+                note: `☒File size is not within filter limits. \\n`
+            }
+            return response
+
+        }
+
+    } catch (err) {
+        var response = {
+            outcome: false,
+            note:  `library.filters.filterBySize error: ${err.stack} \\n`
+        }
+        return response
+    }
+}
+
+module.exports = filterBySize
