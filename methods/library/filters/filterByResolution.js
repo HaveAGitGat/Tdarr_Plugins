1477f1c59c8e551e6292583a89ae999fe766051a
HaveAGitGat <43864057+HaveAGitGat@users.noreply.github.com>
2019-11-29 19:27:41 +0000

    [New] Filter by resolution

---
+++ methods/library/filters/filterByResolution.js
@@ -0,0 +1,58 @@
+
+
+function filterByResolution(file, mode, resolution) {
+
+    try {
+
+        if (mode === 'exclude') {
+
+            if (resolution.toLowerCase().includes(file.video_resolution.toLowerCase())) {
+
+                var response = {
+                    outcome: false,
+                    note: `☒File is in excluded resolution. \\n`
+                }
+                return response
+
+            } else {
+
+                var response = {
+                    outcome: true,
+                    note: `☑File is not in excluded resolution. \\n`
+                }
+                return response
+
+            }
+
+        } else if (mode === 'include') {
+
+            if (resolution.toLowerCase().includes(file.video_resolution.toLowerCase())) {
+
+                var response = {
+                    outcome: true,
+                    note: `☑File is in included resolution. \\n`
+                }
+                return response
+
+            } else {
+
+                var response = {
+                    outcome: false,
+                    note: `☒File is not in included resolution. \\n`
+                }
+                return response
+
+            }
+        }
+
+    } catch (err) {
+        var response = {
+            outcome: false,
+            note:  `library.filters.filterByResolution error: ${err.stack} \\n`
+        }
+        return response
+    }
+}
+
+
+module.exports = filterByResolution
