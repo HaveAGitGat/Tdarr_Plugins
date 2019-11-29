2d41b20eda0b76866dde7c19520367fac5c5307f
HaveAGitGat <43864057+HaveAGitGat@users.noreply.github.com>
2019-11-29 19:29:05 +0000

    [New] Filter by age

---
+++ methods/library/filters/filterByAge.js
@@ -0,0 +1,40 @@
+
+
+function filterByAge(file, ageCutOff_Seconds) {
+
+    try {
+
+        var timeNow = new Date()
+        var dateCreated = new Date(file.statSync.birthtime)
+        var fileAge = Math.round((timeNow - dateCreated) / 1000)
+
+
+        if (fileAge > ageCutOff_Seconds) {
+
+            var response = {
+                outcome: false,
+                note: `☒File creation date is older than specified requirement. \\n`
+            }
+            return response
+
+        } else {
+
+            var response = {
+                outcome: true,
+                note: `☑File creation date is within specified requirement. \\n`
+            }
+            return response
+
+        }
+
+    } catch (err) {
+        var response = {
+            outcome: false,
+            note: `library.filters.filterByAge error: ${err.stack} \\n`
+        }
+        return response
+    }
+}
+
+
+module.exports = filterByAge
