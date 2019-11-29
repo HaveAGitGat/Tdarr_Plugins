2c26bf369221fa52072c6488b3da034bc4beeee9
HaveAGitGat <43864057+HaveAGitGat@users.noreply.github.com>
2019-11-29 19:29:24 +0000

    [New] Action: Remux container

---
+++ methods/library/actions/remuxContainer.js
@@ -0,0 +1,38 @@
+
+
+function remuxContainer(file, container) {
+
+    try {
+
+
+        if (file.container != container) {
+
+            var response = {
+                processFile: true,
+                note:`File is not in ${container} \\n`
+            }
+            return response
+
+        } else {
+
+            var response = {
+                processFile: false,
+                note:`File is already in ${container} \\n`
+            }
+            return response
+
+        }
+
+
+
+    } catch (err) {
+        var response = {
+            processFile: false, 
+            note:`library.actions.remuxContainer error: ${err.stack} \\n`
+        }
+        return response
+    }
+}
+
+
+module.exports = remuxContainer
