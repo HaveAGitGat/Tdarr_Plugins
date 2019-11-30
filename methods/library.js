373207f4d1f5d702514209139926436e95de4b71
HaveAGitGat <43864057+HaveAGitGat@users.noreply.github.com>
2019-11-30 21:12:56 +0000

    Prod path update

--- methods/library.js
+++ methods/library.js
@@ -1,5 +1,13 @@
 
-const importFresh = require('import-fresh');
+var fs = require('fs');
+
+if (fs.existsSync(path.join(process.cwd() + "/npm"))) {
+    var rootModules = path.join(process.cwd() + '/npm/node_modules/')
+} else{
+    var rootModules = ''
+}
+
+const importFresh = require(rootModules+'import-fresh');
 
 var filters = importFresh('./filters.js')
 var actions = importFresh('./actions.js')
