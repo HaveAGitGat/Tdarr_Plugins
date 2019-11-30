a31216beb179b4f19fce66179879f5a46c42b5bb
HaveAGitGat <43864057+HaveAGitGat@users.noreply.github.com>
2019-11-30 21:16:10 +0000

    Modules prod path update

--- methods/actions.js
+++ methods/actions.js
@@ -1,4 +1,11 @@
-const importFresh = require('import-fresh');
+var fs = require('fs');
+var path = require("path");
+if (fs.existsSync(path.join(process.cwd() + "/npm"))) {
+    var rootModules = path.join(process.cwd() + '/npm/node_modules/')
+} else{
+    var rootModules = ''
+}
+const importFresh = require(rootModules+'import-fresh');
 
 
 var remuxContainer = importFresh('./library/actions/remuxContainer.js')
--- methods/library.js
+++ methods/library.js
@@ -1,13 +1,10 @@
-
 var fs = require('fs');
 var path = require("path");
-
 if (fs.existsSync(path.join(process.cwd() + "/npm"))) {
     var rootModules = path.join(process.cwd() + '/npm/node_modules/')
 } else{
     var rootModules = ''
 }
-
 const importFresh = require(rootModules+'import-fresh');
 
 var filters = importFresh('./filters.js')
