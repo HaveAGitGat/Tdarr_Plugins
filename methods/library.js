5dfddbfde0b26b7e0368d885fdc601bde9faa75d
HaveAGitGat <43864057+HaveAGitGat@users.noreply.github.com>
2019-11-30 21:14:25 +0000

    Include path

--- methods/library.js
+++ methods/library.js
@@ -1,5 +1,6 @@
 
 var fs = require('fs');
+var path = require("path");
 
 if (fs.existsSync(path.join(process.cwd() + "/npm"))) {
     var rootModules = path.join(process.cwd() + '/npm/node_modules/')
