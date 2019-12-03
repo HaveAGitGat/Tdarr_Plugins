0b1f06120b4743c36ac4341d0c04dd6efdaf3fac
HaveAGitGat <43864057+HaveAGitGat@users.noreply.github.com>
2019-12-03 16:39:22 +0000

    Link

--- Community/Tdarr_Plugin_075b_FFMPEG_HEVC_Generic_Video_Audio_Only.js
+++ Community/Tdarr_Plugin_075b_FFMPEG_HEVC_Generic_Video_Audio_Only.js
@@ -10,7 +10,7 @@ function details() {
     Operation:"Transcode",
     Description: `[Contains built-in filter] This plugin transcodes non h265 files into h265 mkv using default settings. Only video and audio streams are kept. Useful for if you're getting errors because of certain containers not being able to handle certain subtitle/data streams.  \n\n`,
     Version: "1.00",
-    Link: ""
+    Link: "https://github.com/HaveAGitGat/Tdarr_Plugins/blob/master/Community/Tdarr_Plugin_075b_FFMPEG_HEVC_Generic_Video_Audio_Only.js"
   }
 
 }
