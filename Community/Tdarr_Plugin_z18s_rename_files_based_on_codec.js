8ac2e2f165aea1a8543916c6ecdcd0827a128100
HaveAGitGat <43864057+HaveAGitGat@users.noreply.github.com>
2020-02-14 21:03:17 +0000

    Update Tdarr_Plugin_z18s_rename_files_based_on_codec.js

--- Community/Tdarr_Plugin_z18s_rename_files_based_on_codec.js
+++ Community/Tdarr_Plugin_z18s_rename_files_based_on_codec.js
@@ -22,7 +22,15 @@ module.exports.plugin = function plugin(file, librarySettings, inputs) {
 
   try {
 
-    var fsextra = require('fs-extra')
+    var fs = require('fs');
+    var path = require('path');
+    if (fs.existsSync(path.join(process.cwd() , '/npm'))) {
+    var rootModules = path.join(process.cwd() , '/npm/node_modules/')
+    } else{
+    var rootModules = ''
+    }
+
+    var fsextra = require(rootModules+'fs-extra')
 
     var fileNameOld = file._id
 
