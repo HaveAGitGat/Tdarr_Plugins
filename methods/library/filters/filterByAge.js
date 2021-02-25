5f524dc9305ad0905150f901e7e4e43f99138d29
HaveAGitGat <43864057+HaveAGitGat@users.noreply.github.com>
2021-02-25 05:56:20 +0100

    Fix age filter

--- methods/library/filters/filterByAge.js
+++ methods/library/filters/filterByAge.js
@@ -1,25 +1,24 @@
-/* eslint-disable */
-function filterByAge(file, ageCutOff_Seconds) {
+function filterByAge(file, ageCutOff_Seconds, type) {
   try {
-    var timeNow = new Date();
-    var dateCreated = new Date(file.statSync.birthtime);
-    var fileAge = Math.round((timeNow - dateCreated) / 1000);
+    const timeNow = new Date();
+    const dateCreated = new Date(file.statSync.birthtime);
+    const fileAge = Math.round((timeNow - dateCreated) / 1000);
 
-    if (fileAge > ageCutOff_Seconds) {
-      var response = {
+    if ((type === 'exclude' && fileAge > ageCutOff_Seconds) || (type === 'include' && fileAge < ageCutOff_Seconds)) {
+      const response = {
         outcome: false,
-        note: `☒File creation date is older than specified requirement. \n`,
-      };
-      return response;
-    } else {
-      var response = {
-        outcome: true,
-        note: `☑File creation date is within specified requirement. \n`,
+        note: 'File creation date is not within specified requirement. Wont process.  \n',
       };
       return response;
     }
+
+    const response = {
+      outcome: true,
+      note: 'File creation date is within specified requirement. Will process. \n',
+    };
+    return response;
   } catch (err) {
-    var response = {
+    const response = {
       outcome: false,
       note: `library.filters.filterByAge error: ${err} \n`,
     };
