659ec29c3c7a4c144e399332526c68e3f02389a0
Greg Baker <gregbakerstl@gmail.com>
2021-11-17 17:16:58 -0600

    Tdarr_Plugin_Greg_MP3_FFMPEG_CPU.js
    Trying to contribute a frequently mentioned feature, built and tested on a clustered Tdarr 2.00.10 in Debian/Ubuntu.

---
+++ Community/Tdarr_Plugin_Greg_MP3_FFMPEG_CPU.js
@@ -0,0 +1,51 @@
+module.exports.dependencies = [
+    'import-fresh',
+  ]
+
+  module.exports.details = function details() {
+        return {
+          id: 'Tdarr_Plugin_Greg_MP3_FFMPEG_CPU",
+          Name: "Audio Transcode to MP3 using CPU and FFMPEG",
+          Type: "Audio",
+          Operation: "Transcode",
+          Description: "Convert an audio file to mp3, retaining ID3 tags, and at original bitrate up to 384K - from type of: \"flac,wav,ape,ogg,m4a,wma,opus\"",
+          Version: "0.0.1",
+          Link: "https://github.com/HaveAGitGat/Tdarr_Plugins/blob/master/Community/Tdarr_Plugin_Greg_MP3_FFMPEG_CPU.js"
+					Tags: 'pre-processing,ffmpeg,audio only,mp3',
+        }
+      }
+
+  module.exports.plugin = function plugin(file) {
+
+    const importFresh = require('import-fresh');
+    const library = importFresh('../methods/library.js');
+
+        //Must return this object at some point
+        var response = {
+           processFile : false,
+           preset : '',
+           container : '.mkv',
+           handbrakeMode : false,
+           ffmpegMode : true,
+           reQueueAfter : true,
+           infoLog : '',
+      
+        }
+
+        response.infoLog += "" + library.filters.filterByCodec(file,"include","flac,wav,ape,ogg,m4a,wma,opus").note + library.filters.filterByCodec(file,"exclude","mp3").note
+      
+        if((true &&library.filters.filterByCodec(file,"include","flac,wav,ape,ogg,m4a,wma,opus").outcome === true &&library.filters.filterByCodec(file,"exclude","mp3").outcome === true) || file.forceProcessing === true){
+            response.preset = ', -map_metadata 0 -id3v2_version 3 -b:a 384k'
+            response.container = '.mp3'
+            response.handbrakeMode = false
+            response.ffmpegMode = true
+            response.reQueueAfter = true;
+            response.processFile = library.actions.remuxContainer(file, 'mp3').processFile
+            response.infoLog +=  library.actions.remuxContainer(file, 'mp3').note
+            return response
+           }else{
+            response.processFile = false;
+            response.infoLog += library.actions.remuxContainer(file, 'mp3').note
+            return response
+           }
+      }
