e957a57afd56b07c06568489abbea3ce765619a7
Greg Baker <gregbakerstl@gmail.com>
2021-11-18 12:36:06 -0600

    code review feedback incorporation
    Made many changes based on the review from https://github.com/supersnellehenk !

--- Community/Tdarr_Plugin_Greg_MP3_FFMPEG_CPU.js
+++ Community/Tdarr_Plugin_Greg_MP3_FFMPEG_CPU.js
@@ -3,48 +3,42 @@ const importFresh = require('import-fresh');
 module.exports.details = function details() {
   return {
     id: 'Tdarr_Plugin_Greg_MP3_FFMPEG_CPU',
-    Name: 'Audio Transcode to MP3 using CPU and FFMPEG',
-    Type: 'Audio',
-    Operation: 'Transcode',
     Description: '[Contains built-in filter] Convert an audio file to mp3, retaining ID3 tags, '
-    + 'and at original bitrate up to 384K - from type of: "flac,wav,ape,ogg,m4a,wma,opus" ',
-    Version: '0.0.1',
+    + 'and at original bitrate up to 320k - from type of: "flac,wav,ape,ogg,m4a,wma,opus" ',
     Link: 'https://github.com/HaveAGitGat/Tdarr_Plugins/blob/master/Community/Tdarr_Plugin_Greg_MP3_FFMPEG_CPU.js',
+    Name: 'Audio Transcode to MP3 using CPU and FFMPEG',
+    Operation: 'Transcode',
     Tags: 'pre-processing,ffmpeg,audio only,mp3',
+    Type: 'Audio',
+    Stage: 'Pre-processing',
+    Version: '0.0.1',
   };
 };
 
 module.exports.plugin = function plugin(file) {
   const library = importFresh('../methods/library.js');
 
-  // Must return this object at some point
   const response = {
-    processFile: false,
-    preset: '',
-    container: '.mkv',
+    // 320K selected over 384k intentionally
+    // https://en.m.wikipedia.org/wiki/MPEG-1#Part_3:_Audio
+    preset: ', -map_metadata 0 -id3v2_version 3 -b:a 320k',
+    container: '.mp3',
     handbrakeMode: false,
     ffmpegMode: true,
+    processFile: false,
     reQueueAfter: true,
-    infoLog: '',
-
   };
 
   response.infoLog += `${library.filters.filterByCodec(file, 'include', 'flac,wav,ape,ogg,m4a,wma,opus').note}
   ${library.filters.filterByCodec(file, 'exclude', 'mp3').note}`;
 
-  if ((true && library.filters.filterByCodec(file, 'include', 'flac,wav,ape,ogg,m4a,wma,opus').outcome === true
-    && library.filters.filterByCodec(file, 'exclude', 'mp3').outcome === true)
-    || file.forceProcessing === true) {
-    response.preset = ', -map_metadata 0 -id3v2_version 3 -b:a 384k';
-    response.container = '.mp3';
-    response.handbrakeMode = false;
-    response.ffmpegMode = true;
-    response.reQueueAfter = true;
-    response.processFile = library.actions.remuxContainer(file, 'mp3').processFile;
+  if ((library.filters.filterByCodec(file, 'include', 'flac,wav,ape,ogg,m4a,wma,opus').outcome
+    && library.filters.filterByCodec(file, 'exclude', 'mp3').outcome)
+    || file.forceProcessing) {
     response.infoLog += library.actions.remuxContainer(file, 'mp3').note;
+    response.processFile = library.actions.remuxContainer(file, 'mp3').processFile;
     return response;
   }
-  response.processFile = false;
   response.infoLog += library.actions.remuxContainer(file, 'mp3').note;
   return response;
 };
