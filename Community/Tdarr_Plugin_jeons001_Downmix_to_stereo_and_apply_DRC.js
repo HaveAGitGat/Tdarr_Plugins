8bdf742e1831d2c7ad58540f81c5fb166f672865
jenkal <ja.kaalen@gmail.com>
2021-11-19 11:33:55 +0100

    made adjustments to requirements for processing to only process files with 1 audio stream

--- Community/Tdarr_Plugin_jeons001_Downmix_to_stereo_and_apply_DRC.js
+++ Community/Tdarr_Plugin_jeons001_Downmix_to_stereo_and_apply_DRC.js
@@ -5,7 +5,7 @@ module.exports.details = function details() {
         Name: 'Downmix & Dynamic range compression',
         Type: 'Audio',
         Operation: 'Transcode',
-        Description: 'Downmixes surround to stereo AND applies dynamic range compression. Files already in stereo will be skipped \n\n',
+        Description: 'Downmixes surround to AAC stereo AND applies dynamic range compression. Files already in stereo or with multiple audio tracks will be skipped \n\n',
         Version: '1.00',
         Link: 'https://github.com/HaveAGitGat/Tdarr_Plugins/blob/master/Community/Tdarr_Plugin_jeons001_Downmix_to_stereo_and_apply_DRC.js',
         Tags: 'ffmpeg',
@@ -28,15 +28,35 @@ module.exports.plugin = function plugin(file, librarySettings, inputs) {
         container: `.${file.container}`
     };
 
+    let audioStreams = 0;
+    let surroundTrackFound = false;
+
     for (let index = 0; index < file.ffProbeData.streams.length; index++) {
         const stream = file.ffProbeData.streams[index];
-        if (stream.codec_type === "audio" && stream.channels && stream.channels > 3) {
-            response.preset = '-sn <io> -vcodec copy -scodec copy -acodec aac -af "dynaudnorm,pan=stereo|FL < 1.0*FL + 0.707*FC + 0.707*BL|FR < 1.0*FR + 0.707*FC + 0.707*BR"';
-            response.processFile = true;
-            response.infoLog = "Didn't find file to be in stereo... Downmixing and applying DRC!";
-            return response;
+        if (stream.codec_type === "audio") {
+            audioStreams++;
         }
 
+        if (stream.codec_type === "audio" && stream.channels && stream.channels > 3) {
+            surroundTrackFound = true;
+        }
+    }
+
+    if (audioStreams > 1) {
+        response.infoLog = "File has more than 1 audio track - not processing";
+        return response;
+    }
+
+    if (!surroundTrackFound) {
+        response.infoLog = "File has no surround tracks - not processing";
+        return response;
+    }
+
+    if (surroundTrackFound && audioStreams === 1) {
+        response.preset = '-sn <io> -vcodec copy -scodec copy -acodec aac -filter:a "dynaudnorm,pan=stereo|FL < 1.0*FL + 0.707*FC + 0.707*BL|FR < 1.0*FR + 0.707*FC + 0.707*BR"';
+        response.processFile = true;
+        response.infoLog = "File matches requirements for processing. Downmixing and applying DRC!";
+        return response;
     }
 
     return response;
