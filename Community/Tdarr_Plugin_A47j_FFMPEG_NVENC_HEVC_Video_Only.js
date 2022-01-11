4f976e35e4676e9f9fc31553213f293f1933bd31
Jeff R <jeff@jeffrice.net>
2022-01-11 16:37:31 -0500

    Update Tdarr_Plugin_A47j_FFMPEG_NVENC_HEVC_Video_Only.js
    Updated to make use of mediaInfo file object and dropdowns.  All external dependencies removed.

--- Community/Tdarr_Plugin_A47j_FFMPEG_NVENC_HEVC_Video_Only.js
+++ Community/Tdarr_Plugin_A47j_FFMPEG_NVENC_HEVC_Video_Only.js
@@ -1,17 +1,15 @@
 /* eslint-disable */
 const details = () => {
   return {
-    id: "Tdarr_Plugin_A47j_FFMPEG_NVENC_HEVC_Video_Only",
+    id: "Tdarr_Plugin_A47j_FFMPEG_NVENC_HEVC_VIDEO_Only",
     Stage: 'Pre-processing',
     Name: "FFMPEG nvenc_H265 Video Only",
     Type: "Video",
     Stage: "Pre-processing",
     Operation: "Transcode",
     Description: `[Contains built-in filter] This plugin transcodes non-h265 files into h265 mkv using NVENC, 
-    reducing resolution to 1920x1080 using nvenc. Audio/subtitles not affected. Bitrate is scaled based on input file quality. 
-    == This plugin depends on mediainfo and mkvpropedit, which must be installed manually! 
-    Check this gist for details: https://gist.github.com/jeff47/4ec428e329a485a102bab0398e6ac4be == `,
-    Version: "1.00",
+    reducing resolution to 1920x1080 using nvenc. Audio/subtitles not affected. Bitrate is scaled based on input file quality.`,
+    Version: "1.1",
     Tags: "pre-processing,video only,ffmpeg,nvenc h265,h265",
 
 Inputs: [
@@ -23,8 +21,8 @@ Inputs: [
           type: 'text',
         },
         tooltip: `== Compression Factor == \\n\\n
-        How much does HEVC compress raw video?  I suggest something between 0.04-0.08.  Remember that GPU encoding is not as 
-        efficient as CPU encoding, so resulting file sizes will be larger.\\n\\n
+        How much does HEVC compress raw video?  I suggest something between 0.04-0.08.  Remember that GPU encoding is faster but not as 
+        space efficient as CPU encoding, so resulting file sizes will be larger than if you used CPU encoding.\\n\\n
         0.07 will result in a 1080p@29.92fps having a target bitrate of 5.4mbps.  This is the default.\\n`
       },
       {
@@ -32,11 +30,55 @@ Inputs: [
         type: 'string',
         defaultValue:'false',
         inputUI: {
-          type: 'text',
+          type: 'dropdown',
+          options: [
+          'false',
+          '8KUHD',
+          '4KUHD',
+          '1080p',
+          '720p',
+          '576p',
+          '480p',
+          ],
         },
         tooltip: `== Maximum Resolution ==\\n\\n
         Videos that exceed this resolution will be resized down to this resolution.\\n
-        Accepted options: 480p, 576p, 720p, 1080p, 4KUHD, 8KUHD.  If false, no resizing will occur.\\n`
+        If false, no resizing will occur.\\n`
+      },
+      {
+        name: "ffmpegPreset",
+        type: 'string',
+        defaultValue:'medium',
+        inputUI: {
+          type: 'dropdown',
+          options: [
+            'veryslow',
+            'slower',
+            'slow',
+            'medium',
+            'fast',
+            'faster',
+            'veryfast',
+            'superfast',
+            'ultrafast',
+          ],
+        },
+        tooltip: `== FFmpeg Preset ==\\n\\n
+        Select the ffmpeg preset.\\n`
+      },
+      {
+        name: "container",
+        type: 'string',
+        defaultValue:'mp4',
+        inputUI: {
+          type: 'dropdown',
+          options: [
+            'mp4',
+            'mkv',
+          ],
+        },
+        tooltip: `== Container ==\\n\\n
+        mkv or mp4.\\n`
       },
     ],
 
@@ -52,55 +94,23 @@ var MediaInfo = {
   videoBR: "",
   videoBitDepth: "",
   overallBR: "",
-  JSRProcessed: false,
-  JSRVersion: 0,
-  JSRProcessedTime: 0,
 }; // var MediaInfo
 
 // Easier for our functions if response has global scope.
 var response = {
     processFile: false,
     preset: "",
-    container: ".mkv",
+    container: ".mp4",
     handBrakeMode: false,
     FFmpegMode: true,
     reQueueAfter: true,
     infoLog: "",
 }; // var response
 
-// Runs mkvpropedit --add-track-statistics on the file.
-function updateTrackStats(file) {
-  response.infoLog += `☑Running mkvpropedit.\n`;
-  try {
-    const proc = require("child_process");
-    proc.execFile('mkvpropedit', [ '--delete-track-statistics-tags', file._id], (error,stdout,stderr) => {
-      if (error) throw `mkvpropedit failed: ${error}\n`;
-    });
-    proc.execFile('mkvpropedit', [ '--add-track-statistics-tags', file._id], (error,stdout,stderr) => {
-      if (error) throw `mkvpropedit failed: ${error}\n`;
-    });
-  } catch (err) {
-    response.infoLog += `mkvpropedit failed: ${err}.\n`;
-    throw `mkvpropedit failed: ${err}.\n`;
-  };  // end try/catch
 
-  return 0;
-}  // end updateTrackStats()
 
-// Runs mediainfo on the file, gets JSON output, finds the first video stream and returns the video bit rate and bit depth.
+// Finds the first video stream and populates some useful variables
 function getMediaInfo(file) {
-  var objMedInfo = "";
-
-  response.infoLog += `☑Running mediainfo.\n`;
-
-  try {
-    const proc = require('child_process')
-    objMedInfo = JSON.parse(proc.execFileSync('mediainfo', [file._id,'--output=JSON']));
-  } catch (err) {
-    response.infoLog += `Mediainfo failed: ${err}.\n`;
-    throw `Mediainfo failed: ${err}.\n`;
-  }; // end try/catch
-
   var videoIdx = -1;
   var videoInxFirst = -1;
 
@@ -116,37 +126,19 @@ function getMediaInfo(file) {
 
           MediaInfo.videoHeight = Number(file.ffProbeData.streams[i].height);
           MediaInfo.videoWidth = Number(file.ffProbeData.streams[i].width);
-          MediaInfo.videoFPS = Number(objMedInfo.media.track[i + 1].FrameRate);
-          MediaInfo.videoBR = Number(objMedInfo.media.track[i + 1].BitRate);
-          MediaInfo.videoBitDepth = Number(objMedInfo.media.track[i + 1].BitDepth);
+          MediaInfo.videoFPS = Number(file.mediaInfo.track[i + 1].FrameRate);
+          MediaInfo.videoBR = Number(file.mediaInfo.track[i + 1].BitRate);
+          MediaInfo.videoBitDepth = Number(file.mediaInfo.track[i + 1].BitDepth);
       }
   }
-     MediaInfo.overallBR = objMedInfo.media.track[0].OverallBitRate;
-
-     try {
-       MediaInfo.JSRVersion = Number(objMedInfo.media.track[0].extra.JSRVERSION);
-     } catch (err) {
-       MediaInfo.JSRVersion = "";
-     }
-
-     try {
-       MediaInfo.JSRProcessed = Boolean(objMedInfo.media.track[0].extra.JSRPROCESSED);
-     } catch (err) {
-       MediaInfo.JSRProcessed = "";
-     }
-
-     try {
-       MediaInfo.JSRProcessedTime = Number(objMedInfo.media.track[0].extra.JSRPROCESSEDTIME);
-     } catch (err) {
-       MediaInfo.JSRProcessedTime = "";
-     }
+     MediaInfo.overallBR = file.mediaInfo.track[0].OverallBitRate;
 
   return;
 } // end  getMediaInfo()
 
 // eslint-disable-next-line no-unused-vars
 const plugin = (file, librarySettings, inputs, otherArguments) => {
-    
+
     const lib = require('../methods/lib')();
   // eslint-disable-next-line no-unused-vars,no-param-reassign
   inputs = lib.loadDefaultValues(inputs, details);
@@ -160,11 +152,12 @@ const plugin = (file, librarySettings, inputs, otherArguments) => {
   // How much does HVEC compress the raw stream?
   var compressionFactor = 0.07;
   if ( ! isNaN(Number(inputs.compressionFactor)) ) {
-    compressionFactor = inputs.compressionFactor; 
+    compressionFactor = inputs.compressionFactor;
   } else {
     response.infoLog += `No compression factor selected, defaulting to ${compressionFactor}.\n`;
   }
-  
+
+  response.container = `.${inputs.container}`;
 
   // Do we resize?
   var resolutionOrder = [ "480p", "576p", "720p", "1080p", "4KUHD", "8KUHD" ];
@@ -189,57 +182,8 @@ const plugin = (file, librarySettings, inputs, otherArguments) => {
   }
 
 
-  //     --------------------------------  METADATA UPDATES   --------------------------------
-  // If there is no _STATISTICS_WRITING_DATE_UTC-eng field, then we need to run mkvpropedit and
-  //  rerun mediainfo to load the stats.
-  if (file.ffProbeData.streams[0].tags === undefined || file.ffProbeData.streams[0].tags["_STATISTICS_WRITING_DATE_UTC-eng"] === undefined ) {
-    response.infoLog += "☑Track statistics are missing.\n";
-    updateTrackStats(file);
-    getMediaInfo(file);
-  } else {
-    // mkvpropedit records the time the stats were written.  Get it (specify it is in UTC) and add a 10 second buffer.
-    StatsWritingTime = Date.parse(`${file.ffProbeData.streams[0].tags["_STATISTICS_WRITING_DATE_UTC-eng"]} UTC`) + 10000;
-
-    // If the file's mtime is more than 60 seconds later than  StatsWritingTime, then we should rerun mkvpropedit!
-    if ( file.statSync.mtimeMs > StatsWritingTime ) {
-      response.infoLog += "☑Track statistics are out of date.\n";
-      updateTrackStats(file);
-      getMediaInfo(file);
-    } else {
-      response.infoLog += "☑Track statistics are up to date.\n";
-      getMediaInfo(file);
-    }
-  }
-
-  if ( isNaN(MediaInfo.videoBR) || isNaN(MediaInfo.videoBitDepth) ) {
-    response.infoLog += "videoBR or videoBitDepth was NaN, something went wrong with mediainfo.\n";
-    updateTrackStats(file);
-    getMediaInfo(file);
-    if ( isNaN(MediaInfo.videoBR) || isNaN(MediaInfo.videoBitDepth) ) {
-      response.infoLog += "videoBR or videoBitDepth still NaN, using default.\n";
-    }
-  }
-
-  // If the overall bitrate is less than the videoBR, then something is wacky.
-  if ( MediaInfo.videoBR > MediaInfo.overallBR ) {
-    response.infoLog += `videoBR (${MediaInfo.videoBR} was greater than overallBR (${MediaInfo.overallBR}),
-      which is impossible. Updating stats.\n`;
-    updateTrackStats(file);
-    getMediaInfo(file);
-    if ( MediaInfo.videoBR > MediaInfo.overallBR ) {
-      response.infoLog += `videoBR and overallBR still inconsistent, using default.\n`;
-    }
-  }
-
-
-  if ( file.forceProcessing !== true ) {
-  if ( MediaInfo.JSRProcessed !== undefined && MediaInfo.JSRProcessed == true) {
-    response.infoLog += `JSRPROCESSED metadata tag was true.  This file was already transcoded by this plugin.  Exiting...\n`;
-    response.processFile = false;
-    return response;
-  }
-}
-
+  getMediaInfo(file);
+  
   // Set decoding options here
   switch (file.ffProbeData.streams[0].codec_name) {
     case "hevc":
@@ -283,7 +227,7 @@ if ( isNaN(MediaInfo.videoBR) ) {
   var bufsize = Math.round(MediaInfo.videoBR);
 }
 
-response.preset += `,-map 0:v -map 0:a -map 0:s? -map -:d? -c copy -c:v:0 hevc_nvenc -rc:v vbr_hq -preset medium -profile:v main10 -rc-lookahead 32 -spatial_aq:v 1 -aq-strength:v 8 -max_muxing_queue_size 4096 `;
+response.preset += `,-map 0:v -map 0:a -map 0:s? -map -:d? -c copy -c:v:0 hevc_nvenc -rc:v vbr_hq -preset ${inputs.ffmpegPreset} -profile:v main10 -rc-lookahead 32 -spatial_aq:v 1 -aq-strength:v 8 -max_muxing_queue_size 4096 `;
 response.infoLog += `Video bitrate is ${Math.round(MediaInfo.videoBR/1000)}Kbps, overall is ${Math.round(MediaInfo.overallBR/1000)}Kbps. `;
 response.infoLog += `Calculated target is ${Math.round(targetBitrate/1000)}Kbps.\n`;
 
@@ -331,14 +275,14 @@ response.infoLog += `Calculated target is ${Math.round(targetBitrate/1000)}Kbps.
 
 
   if (response.processFile == true) {
-    response.preset += ` -map_metadata:g -1 -metadata JSRVERSION=1 -metadata JSRPROCESSED=true -metadata JSRPROCESSEDTIME=${Date.now()} `;
+    response.preset += ` -map_metadata:g -1`;
     response.FFmpegMode = true;
     response.infoLog += `☒Transcoding to HEVC.`;
   } else {
-    if (file.container != "mkv") {
+    if (file.container != ".${inputs.container}" ) {
       response_preset = ',-c copy -map 0';
       response.processFile = true;
-      response.infoLog += `☒Remuxing to mkv.`;
+      response.infoLog += `☒Remuxing to ${inputs.container}`;
     }
   }
 
