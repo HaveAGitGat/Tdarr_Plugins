8db9c37038e507b106135a4db17cf2cc5d5067e3
HaveAGitGat <43864057+HaveAGitGat@users.noreply.github.com>
2022-02-07 05:48:56 +0000

    Fix checkPlugins error

--- Community/Tdarr_Plugin_bsh1_Boosh_FFMPEG_QSV_HEVC.js
+++ Community/Tdarr_Plugin_bsh1_Boosh_FFMPEG_QSV_HEVC.js
@@ -6,7 +6,7 @@
 // eslint-disable-next-line max-len
 // https://www.intel.com/content/dam/www/public/us/en/documents/white-papers/cloud-computing-quicksync-video-ffmpeg-white-paper.pdf
 
-function details() {
+const details = () => {
   return {
     id: 'Tdarr_Plugin_bsh1_Boosh_FFMPEG_QSV_HEVC',
     Stage: 'Pre-processing',
