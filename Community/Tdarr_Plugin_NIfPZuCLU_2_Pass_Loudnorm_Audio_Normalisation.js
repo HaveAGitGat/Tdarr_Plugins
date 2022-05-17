2e84c9c921294faa80c79559e4c819713d7b50cf
HaveAGitGat <43864057+HaveAGitGat@users.noreply.github.com>
2022-05-17 19:03:25 +0100

    use ffprobedata

--- Community/Tdarr_Plugin_NIfPZuCLU_2_Pass_Loudnorm_Audio_Normalisation.js
+++ Community/Tdarr_Plugin_NIfPZuCLU_2_Pass_Loudnorm_Audio_Normalisation.js
@@ -96,8 +96,13 @@ const plugin = (file, librarySettings, inputs, otherArguments) => {
     logOutFile = currentfilename.substr(0, currentfilename.lastIndexOf(".")) + ".out"
     console.log("Log out file: " + logOutFile)
 
-    //get an updated version of the file for checking metadata
-    var probeData = JSON.parse(require("child_process").execSync(`ffprobe -v quiet -print_format json -show_format -show_streams "${currentfilename}"`).toString())
+    let probeData;
+    if (file && file.ffProbeData && file.ffProbeData.format) {
+        probeData = file.ffProbeData;
+    } else {
+        //get an updated version of the file for checking metadata
+        probeData = JSON.parse(require("child_process").execSync(`ffprobe -v quiet -print_format json -show_format -show_streams "${currentfilename}"`).toString())
+    }
 
     //setup required varibles
     var loudNorm_i = -23.0
