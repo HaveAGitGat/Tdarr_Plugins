37af626ca4dc14e18c56e5fa730397d8ef700926
HaveAGitGat <43864057+HaveAGitGat@users.noreply.github.com>
2022-05-22 17:26:55 +0100

    Add Tdarr_Plugin_MC93_Migz5ConvertAudio test

--- Community/Tdarr_Plugin_MC93_Migz5ConvertAudio.js
+++ Community/Tdarr_Plugin_MC93_Migz5ConvertAudio.js
@@ -1,5 +1,4 @@
 /* eslint no-plusplus: ["error", { "allowForLoopAfterthoughts": true }] */
-// tdarrSkipTest
 const details = () => ({
   id: 'Tdarr_Plugin_MC93_Migz5ConvertAudio',
   Stage: 'Pre-processing',
