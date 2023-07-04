40d25c56f9a25b504ed3b34bc9729440735499ea
HaveAGitGat <43864057+HaveAGitGat@users.noreply.github.com>
2023-07-04 18:44:45 +0100

    Update tests

--- tests/Community/Tdarr_Plugin_00td_action_transcode.js
+++ tests/Community/Tdarr_Plugin_00td_action_transcode.js
@@ -12,18 +12,18 @@ const tests = [
     },
     output: {
       processFile: true,
-      preset: '<io> -map 0 -c copy -c:v libx265 -cq:v 19 -b:v 795571.5361445782 -minrate 556900.0753012047 -maxrate 1034242.9969879518 -bufsize 1591143.0722891565 -spatial_aq:v 1 -rc-lookahead:v 32 -max_muxing_queue_size 9999 ',
+      preset: '  <io> -map 0 -c copy -c:v libx265 -cq:v 19 -b:v 795571.5361445782 -minrate 556900.0753012047 -maxrate 1034242.9969879518 -bufsize 1591143.0722891565 -spatial_aq:v 1 -rc-lookahead:v 32 -max_muxing_queue_size 9999 ',
       handBrakeMode: false,
       FFmpegMode: true,
       reQueueAfter: true,
-      infoLog: 'Container for output selected as mkv. \n'
-        + 'Current bitrate = 1591143.0722891565 \n'
-        + 'Bitrate settings: \n'
-        + 'Target = 795571.5361445782 \n'
-        + 'Minimum = 556900.0753012047 \n'
-        + 'Maximum = 1034242.9969879518 \n'
-        + 'File is not in hevc. Transcoding. \n',
-      container: '.mkv',
+      infoLog: 'Container for output selected as mkv. \n' +
+        'Current bitrate = 1591143.0722891565 \n' +
+        'Bitrate settings: \n' +
+        'Target = 795571.5361445782 \n' +
+        'Minimum = 556900.0753012047 \n' +
+        'Maximum = 1034242.9969879518 \n' +
+        'File is not in hevc. Transcoding. \n',
+      container: '.mkv'
     },
   },
   {
@@ -65,18 +65,18 @@ const tests = [
     },
     output: {
       processFile: true,
-      preset: '<io> -map 0 -c copy -c:v libx265 -cq:v 19 -b:v 1193357.3042168673 -minrate 835350.1129518071 -maxrate 1551364.4954819276 -bufsize 1591143.0722891565 -spatial_aq:v 1 -rc-lookahead:v 32 -max_muxing_queue_size 9999 ',
+      preset: '  <io> -map 0 -c copy -c:v libx265 -cq:v 19 -b:v 1193357.3042168673 -minrate 835350.1129518071 -maxrate 1551364.4954819276 -bufsize 1591143.0722891565 -spatial_aq:v 1 -rc-lookahead:v 32 -max_muxing_queue_size 9999 ',
       handBrakeMode: false,
       FFmpegMode: true,
       reQueueAfter: true,
-      infoLog: 'Container for output selected as mkv. \n'
-        + 'Current bitrate = 1591143.0722891565 \n'
-        + 'Bitrate settings: \n'
-        + 'Target = 1193357.3042168673 \n'
-        + 'Minimum = 835350.1129518071 \n'
-        + 'Maximum = 1551364.4954819276 \n'
-        + 'File is not in hevc. Transcoding. \n',
-      container: '.mkv',
+      infoLog: 'Container for output selected as mkv. \n' +
+        'Current bitrate = 1591143.0722891565 \n' +
+        'Bitrate settings: \n' +
+        'Target = 1193357.3042168673 \n' +
+        'Minimum = 835350.1129518071 \n' +
+        'Maximum = 1551364.4954819276 \n' +
+        'File is not in hevc. Transcoding. \n',
+      container: '.mkv'
     },
   },
 
