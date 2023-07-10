3d0504985c1c3e4c60d028c60f48760c47ef5f98
tws101 <tws101@users.noreply.github.com>
2023-07-10 10:56:31 -0500

    Add files via upload

---
+++ Community/Tdarr_Plugin_tws101_CleanSubs.js
@@ -0,0 +1,257 @@
+/* eslint no-plusplus: ["error", { "allowForLoopAfterthoughts": true }] */
+const details = () => ({
+    id: 'Tdarr_Plugin_tws101_CleanSubs',
+    Stage: 'Pre-processing',
+    Name: 'tws101 - Clean subtitle streams',
+    Type: 'Subtitle',
+    Operation: 'Transcode',
+    Description: 'This plugin keeps only specified language tracks & can tag tracks with an unknown language, and commentary as well as remove signs and song tracks. \n\n',
+//    Created by Migz
+//    Forked by tws101 
+//    Release Version
+    Version: '1.00',
+    Tags: 'pre-processing,ffmpeg,subtitle only,configurable',
+    Inputs: [{
+      name: 'language',
+      type: 'string',
+      defaultValue: 'eng',
+      inputUI: {
+        type: 'text',
+      },
+      tooltip: `Specify language tag/s here for the subtitle tracks you'd like to keep.
+                      \\nMust follow ISO-639-2 3 letter format. https://en.wikipedia.org/wiki/List_of_ISO_639-2_codes
+                 \\nExample:\\n
+                 eng
+  
+                 \\nExample:\\n
+                 eng,jpn`,
+    },
+    {
+      name: 'commentary',
+      type: 'boolean',
+      defaultValue: false,
+      inputUI: {
+        type: 'dropdown',
+        options: [
+          'false',
+          'true',
+        ],
+      },
+      tooltip: `Specify if subtitle tracks that contain commentary/description should be removed.
+                 \\nExample:\\n
+                 true
+  
+                 \\nExample:\\n
+                 false`,
+    },
+    {
+        name: 'signs_and_songs',
+        type: 'boolean',
+        defaultValue: false,
+        inputUI: {
+          type: 'dropdown',
+          options: [
+            'false',
+            'true',
+          ],
+        },
+        tooltip: `Specify if subtitle tracks that contain Signs and Songs should be removed.
+                   \\nExample:\\n
+                   true
+    
+                   \\nExample:\\n
+                   false`,
+      },
+    {
+      name: 'tag_language',
+      type: 'string',
+      defaultValue: '',
+      inputUI: {
+        type: 'text',
+      },
+      tooltip: `Specify a single language for subtitle tracks with no language or unknown language to be tagged with.
+                      \\nMust follow ISO-639-2 3 letter format. https://en.wikipedia.org/wiki/List_of_ISO_639-2_codes
+                      \\nLeave empty to disable.
+                 \\nExample:\\n
+                 eng
+  
+                 \\nExample:\\n
+                 por`,
+    },
+    ],
+  });
+  
+  // eslint-disable-next-line no-unused-vars
+  const plugin = (file, librarySettings, inputs, otherArguments) => {
+    const lib = require('../methods/lib')();
+    // eslint-disable-next-line no-unused-vars,no-param-reassign
+    inputs = lib.loadDefaultValues(inputs, details);
+    const response = {
+      processFile: false,
+      preset: '',
+      container: `.${file.container}`,
+      handBrakeMode: false,
+      FFmpegMode: true,
+      reQueueAfter: false,
+      infoLog: '',
+    };
+  
+    // Check if file is a video. If it isn't then exit plugin.
+    if (file.fileMedium !== 'video') {
+      // eslint-disable-next-line no-console
+      console.log('File is not video');
+      response.infoLog += '☒File is not video \n';
+      response.processFile = false;
+      return response;
+    }
+  
+    // Check if inputs.language has been configured. If it hasn't then exit plugin.
+    if (inputs.language === '') {
+      response.infoLog
+        += '☒Language/s to keep have not been configured, '
+        + 'please configure required options. Skipping this plugin.  \n';
+      response.processFile = false;
+      return response;
+    }
+  
+    // Set up required variables.
+    const language = inputs.language.split(',');
+    let ffmpegCommandInsert = '';
+    let subtitleIdx = 0;
+    let convert = false;
+  
+    // Go through each stream in the file.
+    for (let i = 0; i < file.ffProbeData.streams.length; i++) {
+      // Catch error here incase the language metadata is completely missing.
+      try {
+        // Check if stream is subtitle
+        // AND checks if the tracks language code does not match any of the languages entered in inputs.language.
+        if (
+          file.ffProbeData.streams[i].codec_type.toLowerCase() === 'subtitle'
+          && language.indexOf(
+            file.ffProbeData.streams[i].tags.language.toLowerCase(),
+          ) === -1
+        ) {
+          ffmpegCommandInsert += `-map -0:s:${subtitleIdx} `;
+          response.infoLog += `☒Subtitle stream 0:s:${subtitleIdx} has unwanted language tag ${file.ffProbeData.streams[
+            i
+          ].tags.language.toLowerCase()}, removing. \n`;
+          convert = true;
+        }
+      } catch (err) {
+        // Error
+      }
+  
+      // Catch error here incase the title metadata is completely missing.
+      try {
+        // Check if inputs.commentary is set to true
+        // AND if stream is subtitle
+        // AND then checks for stream titles with the following "commentary, description, sdh".
+        // Removing any streams that are applicable.
+        if (
+          inputs.commentary === true
+          && file.ffProbeData.streams[i].codec_type.toLowerCase() === 'subtitle'
+          && (file.ffProbeData.streams[i].tags.title
+            .toLowerCase()
+            .includes('commentary')
+            || file.ffProbeData.streams[i].tags.title
+              .toLowerCase()
+              .includes('description')
+            || file.ffProbeData.streams[i].tags.title.toLowerCase().includes('sdh'))
+        ) {
+          ffmpegCommandInsert += `-map -0:s:${subtitleIdx} `;
+          response.infoLog += `☒Subtitle stream 0:s:${subtitleIdx} detected as being descriptive, removing. \n`;
+          convert = true;
+        }
+      } catch (err) {
+        // Error
+      }
+
+      // Catch error here incase the title metadata is completely missing.
+      try {
+        // Check if inputs.sings_songs is set to true
+        // AND if stream is subtitle
+        // AND then checks for stream titles with the following "signs, songs".
+        // Removing any streams that are applicable.
+        if (
+          inputs.signs_and_songs === true
+          && file.ffProbeData.streams[i].codec_type.toLowerCase() === 'subtitle'
+          && (file.ffProbeData.streams[i].tags.title
+            .toLowerCase()
+            .includes('signs')
+            || file.ffProbeData.streams[i].tags.title
+              .toLowerCase()
+              .includes('songs'))
+        ) {
+          ffmpegCommandInsert += `-map -0:s:${subtitleIdx} `;
+          response.infoLog += `☒Subtitle stream 0:s:${subtitleIdx} detected as being sign or song, removing. \n`;
+          convert = true;
+        }
+      } catch (err) {
+        // Error
+      }
+  
+      // Check if inputs.tag_language has something entered.
+      // (Entered means user actually wants something to happen, empty would disable this)
+      // AND checks that stream is subtitle.
+      if (
+        inputs.tag_language !== ''
+        && file.ffProbeData.streams[i].codec_type.toLowerCase() === 'subtitle'
+      ) {
+        // Catch error here incase the metadata is completely missing.
+        try {
+          // Look for subtitle with "und" as metadata language.
+          if (
+            file.ffProbeData.streams[i].tags.language
+              .toLowerCase()
+              .includes('und')
+          ) {
+            ffmpegCommandInsert += `-metadata:s:s:${subtitleIdx} language=${inputs.tag_language} `;
+            response.infoLog
+              += `☒Subtitle stream 0:s:${subtitleIdx} has no language, tagging as ${inputs.tag_language}. \n`;
+            convert = true;
+          }
+        } catch (err) {
+          // Error
+        }
+  
+        // Checks if the tags metadata is completely missing.
+        //  If so this would cause playback to show language as "undefined".
+        // No catch error here otherwise it would never detect the metadata as missing.
+        if (typeof file.ffProbeData.streams[i].tags === 'undefined') {
+          ffmpegCommandInsert += `-metadata:s:s:${subtitleIdx} language=${inputs.tag_language} `;
+          response.infoLog
+            += `☒Subtitle stream 0:s:${subtitleIdx} has no language, tagging as ${inputs.tag_language}. \n`;
+          convert = true;
+        } else if (typeof file.ffProbeData.streams[i].tags.language === 'undefined') {
+        // Checks if the tags.language metadata is completely missing.
+        // If so this would cause playback to show language as "undefined".
+        // No catch error here otherwise it would never detect the metadata as missing
+          ffmpegCommandInsert += `-metadata:s:s:${subtitleIdx} language=${inputs.tag_language} `;
+          response.infoLog
+            += `☒Subtitle stream 0:s:${subtitleIdx} has no language, tagging as ${inputs.tag_language}. \n`;
+          convert = true;
+        }
+      }
+  
+      // Check if stream type is subtitle and increment subtitleIdx if true.
+      if (file.ffProbeData.streams[i].codec_type.toLowerCase() === 'subtitle') {
+        subtitleIdx += 1;
+      }
+    }
+  
+    // Convert file if convert variable is set to true.
+    if (convert === true) {
+      response.processFile = true;
+      response.preset = `, -map 0 ${ffmpegCommandInsert} -c copy -max_muxing_queue_size 9999`;
+      response.container = `.${file.container}`;
+      response.reQueueAfter = true;
+    } else {
+      response.processFile = false;
+      response.infoLog += "☑File doesn't contain subtitle tracks which are unwanted or that require tagging.\n";
+    }
+    return response;
+  };
+  module.exports.details = details;
+  module.exports.plugin = plugin;
+  
---
+++ Community/Tdarr_Plugin_tws101_Extract_Copy_Remove_Subtitles.js
@@ -0,0 +1,301 @@
+// jshint esversion: 6
+// tdarrSkipTest
+
+// Created by tehNiemer with thanks to drpeppershaker for the plugin
+// Tdarr_Plugin_rr01_drpeppershaker_extract_subs_to_SRT which served as the base for this.
+// forked tehNiemer Tdarr_Plugin_TN10_SUBS which this is based on this is a tws101 fork
+// This was forked as its merge request was removed, and it is a good plugin
+const details = () => ({
+  id: 'Tdarr_Plugin_tws101_Extract_Copy_Remove_Subtitles',
+  Stage: 'Pre-processing',
+  Name: 'tws101 - Extract/Copy/Remove embedded subtitles',
+  Type: 'Subtitle',
+  Operation: 'Transcode',
+  Description: 'This plugin will extract/copy/remove embedded text and image based subtitles '
+    + 'according to user defined preferences in one pass inside Tdarr, '
+    + 'S_TEXT/WEBVTT subtitles will be removed as ffmpeg does not handle them properly.\n\n ',
+//    Created by tehNiemer
+//    Forked by tws101 
+//    Release Version
+  Version: '1.01',
+  Tags: 'pre-processing,subtitle only,ffmpeg,configurable',
+  Inputs: [{
+    name: 'language',
+    type: 'string',
+    defaultValue: '',
+    inputUI: {
+      type: 'text',
+    },
+    tooltip: 'Specify language tag(s) here for the subtitle tracks you would like to keep/extract.'
+      + '\\nEnter "all" without quotes to copy/extract all subtitle tracks.'
+      + '\\nMust follow ISO-639-2 3 letter format. https://en.wikipedia.org/wiki/List_of_ISO_639-2_codes.'
+      + '\\nExample: \\neng\\nExample: \\neng,jpn,fre',
+  },
+  {
+    name: 'extract',
+    type: 'boolean',
+    defaultValue: false,
+    inputUI: {
+      type: 'dropdown',
+      options: [
+        'false',
+        'true',
+      ],
+    },
+    tooltip: 'Extract defined language subtitle stream(s) to disk.',
+  },
+  {
+    name: 'overwrite',
+    type: 'boolean',
+    defaultValue: false,
+    inputUI: {
+      type: 'dropdown',
+      options: [
+        'false',
+        'true',
+      ],
+    },
+    tooltip: 'Overwrite existing subtitle files on disk if they exist.',
+  },
+  {
+    name: 'rm_extra_lang',
+    type: 'boolean',
+    defaultValue: false,
+    inputUI: {
+      type: 'dropdown',
+      options: [
+        'false',
+        'true',
+      ],
+    },
+    tooltip: 'Remove extra language subtitle streams from file. '
+      + '\\nLanguage(s) defined above will not be removed.',
+  },
+  {
+    name: 'rm_commentary',
+    type: 'boolean',
+    defaultValue: false,
+    inputUI: {
+      type: 'dropdown',
+      options: [
+        'false',
+        'true',
+      ],
+    },
+    tooltip: 'Remove commentary streams from file.',
+  },
+  {
+    name: 'rm_cc_sdh',
+    type: 'boolean',
+    defaultValue: false,
+    inputUI: {
+      type: 'dropdown',
+      options: [
+        'false',
+        'true',
+      ],
+    },
+    tooltip: 'Remove CC/SDH streams from file.',
+  },
+  {
+    name: 'rm_all',
+    type: 'boolean',
+    defaultValue: false,
+    inputUI: {
+      type: 'dropdown',
+      options: [
+        'false',
+        'true',
+      ],
+    },
+    tooltip: 'Remove all subtitle streams from file.',
+  },
+  ],
+});
+
+// eslint-disable-next-line no-unused-vars
+const plugin = (file, librarySettings, inputs, otherArguments) => {
+  const lib = require('../methods/lib')();
+  const fs = require('fs');
+  // eslint-disable-next-line no-unused-vars,no-param-reassign
+  inputs = lib.loadDefaultValues(inputs, details);
+  // Must return this object at some point in the function else plugin will fail.
+  const response = {
+    processFile: true,
+    preset: '',
+    container: `.${file.container}`,
+    handBrakeMode: false,
+    FFmpegMode: true,
+    reQueueAfter: false,
+    infoLog: '',
+  };
+
+  // Check if file is a video. If it isn't then exit plugin.
+  if (file.fileMedium !== 'video') {
+    // eslint-disable-next-line no-console
+    response.infoLog += '☑ File is not video \n';
+    response.processFile = false;
+    return response;
+  }
+
+  // Make sure file has subtitles.
+  let hasSubs = false;
+  for (let i = 0; i < file.ffProbeData.streams.length; i += 1) {
+    const strStreamType = file.ffProbeData.streams[i].codec_type.toLowerCase();
+    if (strStreamType === 'text' || strStreamType === 'subtitle') {
+      hasSubs = true;
+    }
+  }
+
+  if (hasSubs === true) {
+    response.infoLog += '☑ Found subs!\n';
+  } else {
+    response.infoLog += '☑ No subs in file, skipping!\n';
+    response.processFile = false;
+    return response;
+  }
+
+  // Set global variables.
+  let cmdRemove = '';
+  let cmdExtract = '';
+  const processLanguage = inputs.language.toLowerCase().split(',');
+  const bolExtract = inputs.extract;
+  const bolRemoveCommentary = inputs.rm_commentary;
+  const bolRemoveCC_SDH = inputs.rm_cc_sdh;
+  let bolRemoveUnwanted = inputs.rm_extra_lang;
+  const bolRemoveAll = inputs.rm_all;
+  const bolOverwright = inputs.overwrite;
+
+  const subsArr = file.ffProbeData.streams.filter((row) => row.codec_type.toLowerCase() === ('subtitle'));
+
+  let bolExtractAll = false;
+  if (bolExtract && processLanguage === 'all') {
+    bolExtractAll = true;
+  }
+
+  if (bolRemoveAll) {
+    bolRemoveUnwanted = false;
+  }
+
+  for (let i = 0; i < subsArr.length; i += 1) {
+    // Set per-stream variables
+    const subStream = subsArr[i];
+    const { originalLibraryFile } = otherArguments;
+    let subsFile = '';
+    let lang = '';
+    let title = '';
+    let strDisposition = '';
+    let bolCommentary = false;
+    let bolCC_SDH = false;
+    let bolCopyStream = true;
+    let bolExtractStream = true;
+    let bolTextSubs = false;
+
+    if (subStream && subStream.tags && subStream.tags.language) {
+      lang = subStream.tags.language;
+    }
+
+    if (subStream && subStream.tags && subStream.tags.title) {
+      title = subStream.tags.title;
+    }
+
+    if (subStream.disposition.forced || (title.toLowerCase().includes('forced'))) {
+      strDisposition = '.forced';
+    } else if (subStream.disposition.sdh || (title.toLowerCase().includes('sdh'))) {
+      strDisposition = '.sdh';
+      bolCC_SDH = true;
+    } else if (subStream.disposition.cc || (title.toLowerCase().includes('cc'))) {
+      strDisposition = '.cc';
+      bolCC_SDH = true;
+    } else if (subStream.disposition.commentary || subStream.disposition.description
+      || (title.toLowerCase().includes('commentary')) || (title.toLowerCase().includes('description'))) {
+      strDisposition = '.commentary';
+      bolCommentary = true;
+    }
+
+    // Determine if subtitle should be extracted/copied/removed
+    if (processLanguage.indexOf(lang) !== -1) {
+      if ((bolCommentary && bolRemoveCommentary) || (bolCC_SDH && bolRemoveCC_SDH)) {
+        bolCopyStream = false;
+        bolExtractStream = false;
+      }
+      if (!bolExtract) {
+        bolExtractStream = false;
+      }
+    } else if (bolRemoveUnwanted) {
+      bolCopyStream = false;
+    }
+    if ((processLanguage.indexOf(lang) === -1) && !bolExtractAll) {
+      bolExtractStream = false;
+    }
+
+    // Determine subtitle stream type
+    if (subStream.codec_name === ('subrip' || 'mov_text')) {
+      bolTextSubs = true;
+      response.infoLog += '☑ Text ';
+    } else if (subStream.codec_name === ('S_TEXT/WEBVTT')) {
+      bolCopyStream = false;
+      response.infoLog += '☑ S_TEXT/WEBVTT ';
+    } else {
+      response.infoLog += '☑ Image ';
+    }
+
+    // Build subtitle file names.
+    subsFile = originalLibraryFile.file;
+    subsFile = subsFile.split('.');
+    subsFile[subsFile.length - 2] += `.${lang}${strDisposition}`;
+    subsFile[subsFile.length - 1] = 'srt';
+    subsFile = subsFile.join('.');
+
+    if (subsArr.length !== 0) {
+      const { index } = subStream;
+      response.infoLog += `stream ${index}: ${lang}${strDisposition}. `;
+      if (!bolRemoveAll) {
+        // Copy subtitle stream
+        if (bolCopyStream) {
+          response.infoLog += 'Stream will be copied. ';
+          // Skip/Remove undesired subtitle streams.
+        } else {
+          response.infoLog += 'Stream is unwanted, removing. ';
+          cmdRemove += ` -map -0:${index}`;
+        }
+      }
+      // Verify subtitle track is a format that can be extracted.
+      if (bolExtractStream || bolExtractAll) {
+        // Extract subtitle if it doesn't exist on disk or the option to overwrite is set.
+        if (!bolTextSubs) {
+          response.infoLog += 'Subtitle is not text based, can not extract. ';
+        } else if (fs.existsSync(`${subsFile}`) && !bolOverwright) {
+          response.infoLog += 'External subtitle already exists, will not extract. ';
+        } else {
+          response.infoLog += 'Stream will be extracted to file. ';
+          cmdExtract += ` -map 0:${index} "${subsFile}"`;
+        }
+      }
+      response.infoLog += '\n';
+    }
+  }
+
+  if (cmdRemove === '' && cmdExtract === '' && !bolRemoveAll) {
+    response.infoLog += '☑ Nothing to do, skipping!\n';
+    response.processFile = false;
+    return response;
+  } else {
+    response.infoLog += '☒ Working on the above!\n';
+  }
+
+  // Build process command
+  response.preset = '-y <io>';
+  response.preset += cmdExtract;
+  response.preset += ' -map 0';
+  if (bolRemoveAll) {
+    response.infoLog += '☒ Removing all subtitles!\n';
+    response.preset += ' -map -0:s';
+  }
+  response.preset += ' -c copy';
+  response.preset += cmdRemove;
+  return response;
+};
+
+module.exports.details = details;
+module.exports.plugin = plugin;
