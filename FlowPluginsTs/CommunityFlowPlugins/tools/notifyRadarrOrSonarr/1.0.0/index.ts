c3f5112053404c4845255fe1cc446c3b80f57e12
jeanchristophe.mqt@gmail.com <jeanchristophe.mqt@gmail.com>
2024-02-03 00:28:18 +0100

    Refactored the code and added safeties if a file is not found

--- FlowPluginsTs/CommunityFlowPlugins/tools/notifyRadarrOrSonarr/1.0.0/index.ts
+++ FlowPluginsTs/CommunityFlowPlugins/tools/notifyRadarrOrSonarr/1.0.0/index.ts
@@ -1,4 +1,7 @@
 import {
+  getContainer, getFileAbosluteDir, getFileName,
+} from '../../../../FlowHelpers/1.0.0/fileUtils';
+import {
   IpluginDetails,
   IpluginInputArgs,
   IpluginOutputArgs,
@@ -47,11 +50,11 @@ const details = (): IpluginDetails => ({
         type: 'text',
       },
       tooltip: 'Input your arr host here.'
-      + '\\nExample:\\n'
-      + 'http://192.168.1.1:7878\\n'
-      + 'http://192.168.1.1:8989\\n'
-      + 'https://radarr.domain.com\\n'
-      + 'https://sonarr.domain.com\\n',
+        + '\\nExample:\\n'
+        + 'http://192.168.1.1:7878\\n'
+        + 'http://192.168.1.1:8989\\n'
+        + 'https://radarr.domain.com\\n'
+        + 'https://sonarr.domain.com\\n',
     },
   ],
   outputs: [
@@ -62,6 +65,20 @@ const details = (): IpluginDetails => ({
   ],
 });
 
+interface IGetNewPathDelegates {
+  getIdFromParseRequestResult: (parseRequestResult: any) => string,
+  buildRefreshResquestData: (id: string) => string
+}
+interface IRefreshType {
+  appName: string,
+  contentName: string,
+  delegates: IGetNewPathDelegates
+}
+interface IRefreshTypes {
+  radarr: IRefreshType,
+  sonarr: IRefreshType
+}
+
 const plugin = async (args: IpluginInputArgs): Promise<IpluginOutputArgs> => {
   const lib = require('../../../../../methods/lib')();
   // eslint-disable-next-line @typescript-eslint/no-unused-vars,no-param-reassign
@@ -69,72 +86,69 @@ const plugin = async (args: IpluginInputArgs): Promise<IpluginOutputArgs> => {
 
   const { arr, arr_api_key } = args.inputs;
   const arr_host = String(args.inputs.arr_host).trim();
-
-  const fileName = args.originalLibraryFile?.meta?.FileName || '';
-
   const arrHost = arr_host.endsWith('/') ? arr_host.slice(0, -1) : arr_host;
+  const fileName = getFileName(args.inputFileObj._id);
 
-  const headers = {
-    'Content-Type': 'application/json',
-    'X-Api-Key': arr_api_key,
-    Accept: 'application/json',
+  const refresh = async (refreshType: IRefreshType)
+    : Promise<string> => {
+    let pathWithNewName = '';
+
+    args.jobLog('Going to force scan');
+    args.jobLog(`Refreshing ${refreshType.appName}...`);
+
+    const headers = {
+      'Content-Type': 'application/json',
+      'X-Api-Key': arr_api_key,
+      Accept: 'application/json',
+    };
+
+    // Using parse endpoint to get the movie/serie's id.
+    const parseRequestConfig = {
+      method: 'get',
+      url: `${arrHost}/api/v3/parse?title=${encodeURIComponent(fileName)}`,
+      headers,
+    };
+    const parseRequestResult = await args.deps.axios(parseRequestConfig);
+    const id = refreshType.delegates.getIdFromParseRequestResult(parseRequestResult);
+
+    // Checking that the file has been found.
+    if (id !== '-1') {
+      // Using refresh command endpoint to force rescan.
+      const refreshResquestConfig = {
+        method: 'post',
+        url: `${arrHost}/api/v3/command`,
+        headers,
+        data: refreshType.delegates.buildRefreshResquestData(id)
+      };
+      await args.deps.axios(refreshResquestConfig);
+
+      args.jobLog(`✔ Refreshed ${refreshType.contentName} ${id} in ${refreshType.appName}.`);
+    } else
+      args.jobLog(`No ${refreshType.contentName} with a file named '${fileName}'.`);
+
+    return pathWithNewName;
   };
 
-  args.jobLog('Going to force scan');
-
-  if (arr === 'radarr') {
-    args.jobLog('Refreshing Radarr...');
-
-    const requestConfig = {
-      method: 'get',
-      url: `${arrHost}/api/v3/parse?title=${encodeURIComponent(fileName)}`,
-      headers,
-    };
-
-    const res = await args.deps.axios(requestConfig);
-    const { movieId } = res.data.movie.movieFile;
-
-    const requestConfig2 = {
-      method: 'post',
-      url: `${arrHost}/api/v3/command`,
-      headers,
-      data: JSON.stringify({
-        name: 'RefreshMovie',
-        movieIds: [movieId],
-      }),
-    };
-
-    await args.deps.axios(requestConfig2);
-
-    args.jobLog(`✔ Refreshed movie ${movieId} in Radarr.`);
-  } else if (arr === 'sonarr') {
-    args.jobLog('Refreshing Sonarr...');
-
-    const requestConfig = {
-      method: 'get',
-      url: `${arrHost}/api/v3/parse?title=${encodeURIComponent(fileName)}`,
-      headers,
-    };
-
-    const res = await args.deps.axios(requestConfig);
-    const seriesId = res.data.series.id;
-
-    const requestConfig2 = {
-      method: 'post',
-      url: `${arrHost}/api/v3/command`,
-      headers,
-      data: JSON.stringify({
-        name: 'RefreshSeries',
-        seriesId,
-      }),
-    };
-
-    await args.deps.axios(requestConfig2);
-
-    args.jobLog(`✔ Refreshed series ${seriesId} in Sonarr.`);
-  } else {
-    args.jobLog('No arr specified in plugin inputs.');
+  const refreshTypes: IRefreshTypes = {
+    radarr: {
+      appName: 'Radarr',
+      contentName: 'movie',
+      delegates: {
+        getIdFromParseRequestResult: (parseRequestResult) => String(parseRequestResult.data?.movie?.movieFile?.movieId ?? -1),
+        buildRefreshResquestData: id => JSON.stringify({ name: 'RefreshMovie', movieIds: [id] })
+      }
+    },
+    sonarr: {
+      appName: 'Sonarr',
+      contentName: 'serie',
+      delegates: {
+        getIdFromParseRequestResult: (parseRequestResult) => String(parseRequestResult.data?.series?.id ?? -1),
+        buildRefreshResquestData: id => JSON.stringify({ name: 'RefreshSeries', id })
+      },
+    },
   }
+
+  await refresh(arr === 'radarr' ? refreshTypes.radarr : refreshTypes.sonarr);
 
   return {
     outputFileObj: args.inputFileObj,
