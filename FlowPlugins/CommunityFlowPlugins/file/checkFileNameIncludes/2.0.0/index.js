741e39f588d199edbb3522068e49d213a091f55e
jeanchristophe.mqt@gmail.com <jeanchristophe.mqt@gmail.com>
2024-03-20 17:37:41 +0100

    Removed the possibility for regex patterns to be a list. All patterns must be merged

--- FlowPlugins/CommunityFlowPlugins/file/checkFileNameIncludes/2.0.0/index.js
+++ FlowPlugins/CommunityFlowPlugins/file/checkFileNameIncludes/2.0.0/index.js
@@ -29,8 +29,8 @@ var details = function () { return ({
             tooltip: 'Specify terms to check for in file name using comma seperated list e.g. _720p,_1080p',
         },
         {
-            label: 'Patterns',
-            name: 'patterns',
+            label: 'Pattern (regular expression)',
+            name: 'pattern',
             type: 'string',
             // eslint-disable-next-line no-template-curly-in-string
             defaultValue: '',
@@ -38,7 +38,7 @@ var details = function () { return ({
                 type: 'text',
             },
             // eslint-disable-next-line no-template-curly-in-string
-            tooltip: 'Specify patterns (regex) to check for in file name using comma seperated list e.g. ^Pattern*\.mkv$',
+            tooltip: 'Specify the pattern (regex) to check for in file name e.g. ^Pattern*\.mkv$',
         },
         {
             label: 'Include file directory in check',
@@ -71,15 +71,16 @@ var plugin = function (args) {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars,no-param-reassign
     args.inputs = lib.loadDefaultValues(args.inputs, details);
     var buildArrayInput = function (arrayInput) { var _a, _b; return (_b = (_a = String(arrayInput)) === null || _a === void 0 ? void 0 : _a.trim().split(',')) !== null && _b !== void 0 ? _b : new Array(); };
+    var isAMatch = false;
     var fileName = "".concat(Boolean(args.inputs.includeFileDirectory) ? (0, fileUtils_1.getFileAbosluteDir)(args.inputFileObj._id) + '/' : '').concat((0, fileUtils_1.getFileName)(args.inputFileObj._id), ".").concat((0, fileUtils_1.getContainer)(args.inputFileObj._id));
     var searchCriteriasArray = buildArrayInput(args.inputs.terms)
-        .map(function (term) { return term.replace(/[\-\/\\^$*+?.()|[\]{}]/g, '\\$&'); }) // https://github.com/tc39/proposal-regex-escaping
-        .concat(buildArrayInput(args.inputs.patterns));
-    var isAMatch = false;
+        .map(function (term) { return term.replace(/[\-\/\\^$*+?.()|[\]{}]/g, '\\$&'); }); // https://github.com/tc39/proposal-regex-escaping
+    if (args.inputs.pattern)
+        searchCriteriasArray.push(String(args.inputs.pattern));
     for (var i = 0; i < searchCriteriasArray.length; i++)
         if (new RegExp(searchCriteriasArray[i]).test(fileName)) {
             isAMatch = true;
-            args.jobLog("".concat(fileName, " includes ").concat(searchCriteriasArray[i]));
+            args.jobLog("'".concat(fileName, "' includes '").concat(searchCriteriasArray[i], "'"));
             break;
         }
     return {
