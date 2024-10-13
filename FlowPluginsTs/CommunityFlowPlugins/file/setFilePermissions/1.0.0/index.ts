53ca3d12933591607469ddcf3f8cb52aaf946f03
psusen <phil-susen@gmx.de>
2024-10-13 21:14:17 +0200

    add file permission flow plugin

---
+++ FlowPluginsTs/CommunityFlowPlugins/file/setFilePermissions/1.0.0/index.ts
@@ -0,0 +1,59 @@
+import { promises as fsp } from 'fs';
+import {
+  IpluginDetails,
+  IpluginInputArgs,
+  IpluginOutputArgs,
+} from '../../../../FlowHelpers/1.0.0/interfaces/interfaces';
+
+/* eslint no-plusplus: ["error", { "allowForLoopAfterthoughts": true }] */
+const details = ():IpluginDetails => ({
+  name: 'Set File Permissions',
+  description: 'Set the Permissions of the working file to the requested octal permissions. ',
+  style: {
+    borderColor: 'green',
+  },
+  tags: '',
+  isStartPlugin: false,
+  pType: '',
+  requiresVersion: '2.11.01',
+  sidebarPosition: -1,
+  icon: '',
+  inputs: [
+    {
+      label: 'Permission Mode',
+      name: 'permissionMode',
+      type: 'string',
+      defaultValue: '0644',
+      inputUI: {
+        type: 'text',
+      },
+      tooltip: 'Specify the file permissions for the file',
+    },
+  ],
+  outputs: [
+    {
+      number: 1,
+      tooltip: 'Continue to next plugin',
+    },
+  ],
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const plugin = async (args:IpluginInputArgs):Promise<IpluginOutputArgs> => {
+  const lib = require('../../../../../methods/lib')();
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars,no-param-reassign
+  args.inputs = lib.loadDefaultValues(args.inputs, details);
+  const mode = parseInt(String(args.inputs.permissionMode),8);
+  args.jobLog(`Setting ${args.inputFileObj._id} to mode ${mode}`);
+  await fsp.chmod(args.inputFileObj._id,mode);
+
+  return {
+    outputFileObj: args.inputFileObj,
+    outputNumber: 1,
+    variables: args.variables,
+  };
+};
+export {
+  details,
+  plugin,
+};
