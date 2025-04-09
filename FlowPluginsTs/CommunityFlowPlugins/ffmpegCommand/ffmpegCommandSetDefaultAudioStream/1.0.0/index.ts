44420e9e432a18e9e8654a7a11b2d4e319c3093f
jeanchristophe.mqt@gmail.com <jeanchristophe.mqt@gmail.com>
2025-04-09 11:15:14 +0200

    Instructions improved

--- FlowPluginsTs/CommunityFlowPlugins/ffmpegCommand/ffmpegCommandSetDefaultAudioStream/1.0.0/index.ts
+++ FlowPluginsTs/CommunityFlowPlugins/ffmpegCommand/ffmpegCommandSetDefaultAudioStream/1.0.0/index.ts
@@ -24,7 +24,7 @@ const details = (): IpluginDetails => ({
       type: 'string',
       defaultValue: 'eng',
       inputUI: { type: 'text' },
-      tooltip: 'Specify what language to use in the ISO 639-2 format.\nExample:\neng\nfre',
+      tooltip: 'Specify what language to use in the ISO 639-2 format. If the setFlowVariablesFromRadarrOrSonarr plugin is run before this one you can use the default language variable. \nExample:\neng\nfre\{{{args.variables.user.ArrOriginalLanguageCode}}}',
     },
     {
       label: 'Use the highest number of channels as default',
