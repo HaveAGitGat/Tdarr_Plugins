a0dad59aabc62cc6b3859bd141dd121159485427
HaveAGitGat <43864057+HaveAGitGat@users.noreply.github.com>
2025-10-06 07:00:13 +0100

    chore: compile plugin locally

--- FlowPlugins/CommunityFlowPlugins/tools/arithmeticFlowVariable/1.0.0/index.js
+++ FlowPlugins/CommunityFlowPlugins/tools/arithmeticFlowVariable/1.0.0/index.js
@@ -3,46 +3,46 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.plugin = exports.details = void 0;
 /* eslint no-plusplus: ["error", { "allowForLoopAfterthoughts": true }] */
 var details = function () { return ({
-    name: "Arithmetic Flow Variable",
-    description: "Apply an arithmetic calculation on a Flow Variable.",
+    name: 'Arithmetic Flow Variable',
+    description: 'Apply an arithmetic calculation on a Flow Variable.',
     style: {
-        borderColor: "green",
+        borderColor: 'green',
     },
-    tags: "",
+    tags: '',
     isStartPlugin: false,
-    pType: "",
-    requiresVersion: "2.11.01",
+    pType: '',
+    requiresVersion: '2.11.01',
     sidebarPosition: 1,
-    icon: "",
+    icon: '',
     inputs: [
         {
-            label: "Variable",
-            name: "variable",
-            type: "string",
-            defaultValue: "",
+            label: 'Variable',
+            name: 'variable',
+            type: 'string',
+            defaultValue: '',
             inputUI: {
-                type: "text",
+                type: 'text',
             },
             tooltip: "Variable to set.\n      \n      \\nExample\\n\n      transcodeStage\n      ",
         },
         {
-            label: "Operation",
-            name: "operation",
-            type: "string",
-            defaultValue: "",
+            label: 'Operation',
+            name: 'operation',
+            type: 'string',
+            defaultValue: '',
             inputUI: {
-                type: "dropdown",
-                options: ["+", "-", "*", "/"],
+                type: 'dropdown',
+                options: ['+', '-', '*', '/'],
             },
-            tooltip: "Operation to perform on the variable",
+            tooltip: 'Operation to perform on the variable',
         },
         {
-            label: "Quantity",
-            name: "quantity",
-            type: "string",
-            defaultValue: "",
+            label: 'Quantity',
+            name: 'quantity',
+            type: 'string',
+            defaultValue: '',
             inputUI: {
-                type: "text",
+                type: 'text',
             },
             tooltip: "Value to set.\n\n      \\nExample\\n\n      1\n      ",
         },
@@ -50,14 +50,14 @@ var details = function () { return ({
     outputs: [
         {
             number: 1,
-            tooltip: "Continue to next plugin",
+            tooltip: 'Continue to next plugin',
         },
     ],
 }); };
 exports.details = details;
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 var plugin = function (args) {
-    var lib = require("../../../../../methods/lib")();
+    var lib = require('../../../../../methods/lib')();
     // eslint-disable-next-line @typescript-eslint/no-unused-vars,no-param-reassign
     args.inputs = lib.loadDefaultValues(args.inputs, details);
     var variable = String(args.inputs.variable).trim();
@@ -66,24 +66,24 @@ var plugin = function (args) {
     var value = parseInt(args.variables.user[variable], 10);
     args.jobLog("Applying the operation ".concat(operation, " ").concat(quantity, " to ").concat(variable, " of value ").concat(value));
     switch (operation) {
-        case "+":
+        case '+':
             // eslint-disable-next-line no-param-reassign
             args.variables.user[variable] = String(value + quantity);
             break;
-        case "-":
+        case '-':
             // eslint-disable-next-line no-param-reassign
             args.variables.user[variable] = String(value - quantity);
             break;
-        case "*":
+        case '*':
             // eslint-disable-next-line no-param-reassign
             args.variables.user[variable] = String(value * quantity);
             break;
-        case "/":
+        case '/':
             // eslint-disable-next-line no-param-reassign
             args.variables.user[variable] = String(value / quantity);
             break;
         default:
-            throw new Error("The operation ".concat(operation, " is invalid"));
+            throw new Error('The operation '.concat(operation, ' is invalid'));
     }
     return {
         outputFileObj: args.inputFileObj,
