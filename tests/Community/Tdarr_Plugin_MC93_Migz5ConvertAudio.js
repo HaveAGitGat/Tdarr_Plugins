4738bbd27f2b582d119701eac20da0bd494a5a53
HaveAGitGat <43864057+HaveAGitGat@users.noreply.github.com>
2026-05-06 10:40:53 +0100

    test: cover preserved title deduping

--- tests/Community/Tdarr_Plugin_MC93_Migz5ConvertAudio.js
+++ tests/Community/Tdarr_Plugin_MC93_Migz5ConvertAudio.js
@@ -362,6 +362,33 @@ const tests = [
       file: (() => {
         const file = _.cloneDeep(require('../sampleData/media/sampleH264_2.json'));
         file.ffProbeData.streams[1].channels = 8;
+        file.ffProbeData.streams[1].tags.title = 'E-AC-3 Atmos 5.1';
+        return file;
+      })(),
+      librarySettings: {},
+      inputs: {
+        aac_stereo: 'false',
+        downmix: 'true',
+        preserve_channel_title: 'true',
+      },
+      otherArguments: {},
+    },
+    output: {
+      processFile: true,
+      container: '.mkv',
+      handBrakeMode: false,
+      FFmpegMode: true,
+      reQueueAfter: true,
+      infoLog: '☒Audio track is 8 channel, no 6 channel exists. Creating 6 channel "E-AC-3 Atmos 5.1" from 8 channel. \n',
+      preset: ', -map 0 -c:v copy -c:a copy -map 0:1 -c:a:0 ac3 -ac 6 -metadata:s:a:0 "title=E-AC-3 Atmos 5.1" -metadata:s:a:0 "language=eng"  -strict -2 -c:s copy -max_muxing_queue_size 9999 ',
+    },
+  },
+
+  {
+    input: {
+      file: (() => {
+        const file = _.cloneDeep(require('../sampleData/media/sampleH264_2.json'));
+        file.ffProbeData.streams[1].channels = 8;
         file.ffProbeData.streams[2].channels = 8;
         file.ffProbeData.streams[3].channels = 6;
         file.ffProbeData.streams[4].channels = 6;
